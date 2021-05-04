ecfd448fc6285b49415e36031a3209020c2ba0a6
vulxin <zh.kan.zh@gmail.com>
2021-05-04 09:04:05 +0800

    Site updated: 2021-05-04 09:04:02

--- inject/custom.js
+++ inject/custom.js
@@ -1,13 +1,1 @@
-
-
-// 去掉banner图
-// var full_page = document.getElementsByClassName("full_page");
-// if (full_page.length != 0) {
-//   full_page[0].style.background = "transparent";
-// }
-
-// 右键菜单
-window.document.oncontextmenu = function(event) {
-    //return event.ctrlKey;
-    return false;
-};
+"use strict";window.document.oncontextmenu=function(n){return!1};
